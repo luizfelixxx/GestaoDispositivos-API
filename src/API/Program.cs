d237b67f0b7a79f95119f85e25b96219ed501101
LuizFelixxx <luizfelix.dev@gmail.com>
2025-06-19 15:47:58 -0300

    :bug: fix(api): configure JSON serialization for enums

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -11,7 +11,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // --- Configuração dos Serviços ---
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+});
 builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen(); 
 
